0419dfb140ddf198747150b85a5aeea3a5d51514
Danilo Fruttaldo <dfruttaldo@altitudo.com>
2022-05-31 01:00:05 +0200

    Add email sender

--- src/shared/functions.ts
+++ src/shared/functions.ts
@@ -1,5 +1,6 @@
+import { IEmail } from '../models/emailModel';
 import logger from 'jet-logger';
-
+import nodemailer from 'nodemailer';
 
 /**
  * Print an error object if it's truthy. Useful for testing.
@@ -32,5 +33,37 @@ export function newGuid() {
         var r = Math.random() * 16 | 0,
             v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
+    });
+}
+
+/**
+ * Send an email
+ * 
+ * @param email 
+ */
+export function sendMail(email: IEmail) {
+    let transport = nodemailer.createTransport({
+        host: process.env.NODE_MAILER_HOST,
+        port: Number(process.env.NODE_MAILER_PORT),
+        auth: {
+            user: process.env.NODE_MAILER_USER,
+            pass: process.env.NODE_MAILER_PASS
+        }
+    });
+
+    let mailOptions = {
+        from: 'system@mail.com',
+        to: email.to,
+        cc: email.cc,
+        ccn: email.ccn,
+        subject: email.subject,
+        text: email.body
+    };
+
+    transport.sendMail(mailOptions, function (error: any, info: any) {
+        if (error)
+            logger.err(error, true);
+        else
+            logger.info(info);
     });
 }
